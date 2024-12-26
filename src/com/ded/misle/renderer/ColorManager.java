c333c685d82bf4bd0297ec4d7553a02f7741d27b
dedetive <ded31459@hotmail.com>
2024-12-26 11:24:53 -0300

    Point all equivalent shadow colors to a single defaultShadowColor constant

--- src/com/ded/misle/renderer/ColorManager.java
+++ src/com/ded/misle/renderer/ColorManager.java
@@ -4,12 +4,16 @@ import java.awt.*;
 
 public class ColorManager {
 
+    // Shadows
+
+    public final static Color defaultShadowColor = Color.BLACK;
+
     // Floating text
 
     public final static Color damageColor = new Color(0xDE4040);
     public final static Color healColor = new Color(0x50EE50);
     public final static Color entropyGainColor = new Color(0xA0A0FF);
-    public final static Color floatingTextShadow = Color.BLACK;
+    public final static Color floatingTextShadow = defaultShadowColor;
 
     // Boxes
 
@@ -20,17 +24,17 @@ public class ColorManager {
     public final static Color selectedSlotOverlay = new Color(255, 255, 255, 100);
     public final static Color backgroundOverlay = new Color(15, 15, 15, 130);
     public final static Color statColor = new Color(230, 230, 180);
-    public final static Color statShadowColor = Color.BLACK;
-    public final static Color itemCountShadowColor = Color.BLACK;
+    public final static Color statShadowColor = defaultShadowColor;
+    public final static Color itemCountShadowColor = defaultShadowColor;
     public final static Color itemCountColor = Color.WHITE;
-    public final static Color selectedItemNameShadowColor = Color.BLACK;
+    public final static Color selectedItemNameShadowColor = defaultShadowColor;
         // Tooltip
     public final static Color hoveredTooltipBackground = new Color(84, 84, 84, 190);
     public final static Color itemTypeTooltip = new Color(0xE0DE9B);
     public final static Color itemCountTooltip = new Color(0xFFFFFF);
     public final static Color itemEffectTooltip = new Color(0x00A2FF);
     public final static Color itemDescriptionTooltip = new Color(0xA0A0A0);
-    public final static Color tooltipTextShadowColor = Color.BLACK;
+    public final static Color tooltipTextShadowColor = defaultShadowColor;
 
     // Game UI
         // Health UI
@@ -43,16 +47,16 @@ public class ColorManager {
     public final static Color buttonDefaultColor = new Color(70, 51, 5);
     public final static Color buttonHoveredColor = new Color(40, 25, 1);
     public final static Color buttonTextColor = new Color(225, 225, 225);
-    public final static Color buttonTextShadowColor = Color.BLACK;
+    public final static Color buttonTextShadowColor = defaultShadowColor;
         // Menus themselves
     public final static Color menuBackgroundColor = new Color(140, 110, 70);
     public final static Color menuTitleColor = new Color(233, 233, 233);
-    public final static Color menuTitleShadowColor = Color.BLACK;
+    public final static Color menuTitleShadowColor = defaultShadowColor;
     public final static Color menuVersionColor = new Color(217, 217, 217);
-    public final static Color menuVersionShadowColor = Color.BLACK;
+    public final static Color menuVersionShadowColor = defaultShadowColor;
     public final static Color progressBarColor = new Color(100, 200, 100);
     public final static Color progressBarPercentage = new Color(191, 191, 191);
-    public final static Color progressBarPercentageShadow = Color.BLACK;
+    public final static Color progressBarPercentageShadow = defaultShadowColor;
 
     // Fading
 
