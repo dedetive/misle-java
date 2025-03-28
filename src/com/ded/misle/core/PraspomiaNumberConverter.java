ca784f4faa37ee3820cea1b19f2782631a3c9eba
dedetive <ded31459@hotmail.com>
2025-03-28 10:54:47 -0300

    Add PraspomiaNumberConverter class

---
+++ src/com/ded/misle/core/PraspomiaNumberConverter.java
@@ -0,0 +1,70 @@
+package com.ded.misle.core;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+
+public class PraspomiaNumberConverter {
+    static HashMap<String, Integer> praspomiaNumbers = new HashMap<>();
+    static {
+        praspomiaNumbers.put(".", 1);
+        praspomiaNumbers.put("|", 2);
+        praspomiaNumbers.put("7", 3);
+        praspomiaNumbers.put(";", 0);
+    }
+
+    public enum ConvertMode {
+        TO_PRASPOMIA,
+        TO_ARABIC,
+    }
+
+    static final char[] PRASPOMIA_SYMBOLS = {'.', '|', '7'};
+
+    public static String convertNumberSystem(String inputNumber, ConvertMode mode) {
+        switch (mode) {
+            case TO_PRASPOMIA -> {
+                try {
+                    BigInteger toBeConverted = new BigInteger(inputNumber);
+                    return convertToPraspomia(toBeConverted);
+                } catch (NumberFormatException e) {
+                    return "";
+                }
+            }
+
+            case TO_ARABIC -> {
+                try {
+                    return String.valueOf(convertToBase10(inputNumber));
+                } catch (NumberFormatException e) {
+                    return "";
+                }
+            }
+        }
+        return "";
+    }
+
+    private static String convertToPraspomia(BigInteger number) {
+        if (number.compareTo(BigInteger.ZERO) <= 0) {
+            return "Invalid input!";
+        }
+
+        StringBuilder praspomia = new StringBuilder();
+        while (number.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger remainder = number.subtract(BigInteger.ONE).mod(BigInteger.valueOf(3));
+            praspomia.append(PRASPOMIA_SYMBOLS[remainder.intValue()]);
+            number = number.subtract(BigInteger.ONE).divide(BigInteger.valueOf(3));
+        }
+
+        return praspomia.toString();
+    }
+
+    private static BigInteger convertToBase10(String number) {
+        String[] splitNumber = number.split("");
+        BigInteger outputNumber = BigInteger.ZERO;
+
+        for (int i = 0; i < number.length(); i++) {
+            int value = praspomiaNumbers.get(splitNumber[i]);
+            outputNumber = outputNumber.add(BigInteger.valueOf(value).multiply(BigInteger.valueOf(3).pow(i)));
+        }
+
+        return outputNumber;
+    }
+}
