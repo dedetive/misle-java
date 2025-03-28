794019079c9ed90500d958a1c39b7bd282e810ca
dedetive <ded31459@hotmail.com>
2025-03-28 14:28:34 -0300

    Handle ConcurrentModificationException in MenuButton

--- src/com/ded/misle/renderer/MenuButton.java
+++ src/com/ded/misle/renderer/MenuButton.java
@@ -2,17 +2,14 @@ package com.ded.misle.renderer;
 
 import com.ded.misle.core.GamePanel;
 import com.ded.misle.core.LanguageManager;
-import com.ded.misle.core.PraspomiaNumberConverter;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Objects;
 
 import static com.ded.misle.core.GamePanel.deltaTime;
 import static com.ded.misle.core.GamePanel.gameState;
@@ -148,56 +145,60 @@ public class MenuButton {
     private final static int buttonBorderOffsetSize = buttonBorderOffsetPos * 2;
 
     public static void drawButtons(Graphics2D g2d) {
-        for (MenuButton button : buttons) {
-            int buttonBorderSize = (int) (18.4 * scale);
+        try {
+            for (MenuButton button : buttons) {
+                int buttonBorderSize = (int) (18.4 * scale);
 
-            // BORDER
-            g2d.setColor(buttonBorderColor);
-            g2d.fillRoundRect(button.bounds.x - buttonBorderOffsetPos, button.bounds.y - buttonBorderOffsetPos,
-                button.bounds.width + buttonBorderOffsetSize, button.bounds.height + buttonBorderOffsetSize,
-                buttonBorderSize + buttonBorderOffsetPos / 2, buttonBorderSize + buttonBorderOffsetPos / 2);
+                // BORDER
+                g2d.setColor(buttonBorderColor);
+                g2d.fillRoundRect(button.bounds.x - buttonBorderOffsetPos, button.bounds.y - buttonBorderOffsetPos,
+                    button.bounds.width + buttonBorderOffsetSize, button.bounds.height + buttonBorderOffsetSize,
+                    buttonBorderSize + buttonBorderOffsetPos / 2, buttonBorderSize + buttonBorderOffsetPos / 2);
 
-            // BUTTON
-            g2d.setColor(button.color);
-            g2d.fillRoundRect(button.bounds.x, button.bounds.y, button.bounds.width, button.bounds.height,
-                buttonBorderSize, buttonBorderSize);
-
-            // TEXT SHADOW
-            g2d.setFont(buttonFont);
-            FontMetrics fm = g2d.getFontMetrics();
-            int textWidth;
-            if (getCurrentScript() == LanguageManager.Script.PRASPOMIC) {
-                textWidth = fm.stringWidth(removeColorIndicators(impureConvertNumberSystem(button.text, TO_PRASPOMIA)));
-            } else {
-                textWidth = fm.stringWidth(removeColorIndicators(button.text));
-            }
-            int textHeight = fm.getAscent();
-            int textX = button.bounds.x + (button.bounds.width - textWidth) / 2;
-            int textY = button.bounds.y + (button.bounds.height + textHeight) / 2 - fm.getDescent() + (int) (2 * scale);
-            g2d.setColor(buttonTextShadowColor);
-            // Left, right, up, down
-            drawColoredText(g2d, button.text, (int) (textX - textShadow), textY, g2d.getFont(), buttonTextShadowColor, true);
-            drawColoredText(g2d, button.text, (int) (textX + textShadow), textY, g2d.getFont(), buttonTextShadowColor, true);
-            drawColoredText(g2d, button.text, textX, (int) (textY - textShadow), g2d.getFont(), buttonTextShadowColor, true);
-            drawColoredText(g2d, button.text, textX, (int) (textY + textShadow), g2d.getFont(), buttonTextShadowColor, true);
-
-            // ACTUAL TEXT
-            drawColoredText(g2d, button.text, textX, textY, g2d.getFont(), buttonTextColor, false);
-
-            // FADING
-            if (fadingState.containsKey(button.id)) {
-                float progress = fadingProgress.get(button.id);
-                Color fadingColor = new Color((float) buttonFadingColor.getRed() / 256, (float) buttonFadingColor.getGreen() / 256,
-                    (float) buttonFadingColor.getBlue() / 256, progress);
-                g2d.setColor(fadingColor);
+                // BUTTON
+                g2d.setColor(button.color);
                 g2d.fillRoundRect(button.bounds.x, button.bounds.y, button.bounds.width, button.bounds.height,
                     buttonBorderSize, buttonBorderSize);
 
-                fadingProgress.put(button.id, Math.max((float) (progress - 0.015 * deltaTime * 80), 0));
-                if (progress <= 0) {
-                    fadingState.put(button.id, MainRenderer.FadingState.UNFADED);
+                // TEXT SHADOW
+                g2d.setFont(buttonFont);
+                FontMetrics fm = g2d.getFontMetrics();
+                int textWidth;
+                if (getCurrentScript() == LanguageManager.Script.PRASPOMIC) {
+                    textWidth = fm.stringWidth(removeColorIndicators(impureConvertNumberSystem(button.text, TO_PRASPOMIA)));
+                } else {
+                    textWidth = fm.stringWidth(removeColorIndicators(button.text));
+                }
+                int textHeight = fm.getAscent();
+                int textX = button.bounds.x + (button.bounds.width - textWidth) / 2;
+                int textY = button.bounds.y + (button.bounds.height + textHeight) / 2 - fm.getDescent() + (int) (2 * scale);
+                g2d.setColor(buttonTextShadowColor);
+                // Left, right, up, down
+                drawColoredText(g2d, button.text, (int) (textX - textShadow), textY, g2d.getFont(), buttonTextShadowColor, true);
+                drawColoredText(g2d, button.text, (int) (textX + textShadow), textY, g2d.getFont(), buttonTextShadowColor, true);
+                drawColoredText(g2d, button.text, textX, (int) (textY - textShadow), g2d.getFont(), buttonTextShadowColor, true);
+                drawColoredText(g2d, button.text, textX, (int) (textY + textShadow), g2d.getFont(), buttonTextShadowColor, true);
+
+                // ACTUAL TEXT
+                drawColoredText(g2d, button.text, textX, textY, g2d.getFont(), buttonTextColor, false);
+
+                // FADING
+                if (fadingState.containsKey(button.id)) {
+                    float progress = fadingProgress.get(button.id);
+                    Color fadingColor = new Color((float) buttonFadingColor.getRed() / 256, (float) buttonFadingColor.getGreen() / 256,
+                        (float) buttonFadingColor.getBlue() / 256, progress);
+                    g2d.setColor(fadingColor);
+                    g2d.fillRoundRect(button.bounds.x, button.bounds.y, button.bounds.width, button.bounds.height,
+                        buttonBorderSize, buttonBorderSize);
+
+                    fadingProgress.put(button.id, Math.max((float) (progress - 0.015 * deltaTime * 80), 0));
+                    if (progress <= 0) {
+                        fadingState.put(button.id, MainRenderer.FadingState.UNFADED);
+                    }
                 }
             }
+        } catch (ConcurrentModificationException e) {
+            clearButtons();
         }
     }
 
