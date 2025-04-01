e2c71e8ad3d6938f46d660a071a2385ec7499438
dedetive <ded31459@hotmail.com>
2025-04-01 12:44:00 -0300

    Divide reloadSpawnpoint() values by 20 to correctly portray new world coordinates

--- src/com/ded/misle/world/player/PlayerPosition.java
+++ src/com/ded/misle/world/player/PlayerPosition.java
@@ -63,8 +63,8 @@ public class PlayerPosition {
 
 			// These should be the positions the spawnpoints are located
 
-			case 1 -> new int[]{500, 540};
-			case 2 -> new int[]{261, 41};
+			case 1 -> new int[]{25, 27};
+			case 2 -> new int[]{13, 2};
 			default -> new int[]{0, 0};
 		};
 		player.setX(spawnpointCoordinates[0]);
