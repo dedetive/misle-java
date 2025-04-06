462e1dcdd9965cc54071c40f53f3bfb977003ec1
dedetive <ded31459@hotmail.com>
2025-04-06 14:36:42 -0300

    Fix incorrectly grasping player direction

--- src/com/ded/misle/core/PhysicsEngine.java
+++ src/com/ded/misle/core/PhysicsEngine.java
@@ -38,11 +38,11 @@ public class PhysicsEngine {
 	 * @param y double - How many pixels in y direction (this is not based on scale).
 	 */
 	public static void movePlayer(int x, int y) {
+		player.stats.increaseDistance(x, y);
 		x = player.getX() + x;
 		y = player.getY() + y;
 		player.setX(x);
 		player.setY(y);
-		player.stats.increaseDistance(x, y);
 
 //		if (!levelDesigner) {
 //			// Select or unselect NPCs
