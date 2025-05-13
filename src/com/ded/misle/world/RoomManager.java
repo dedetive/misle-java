9c07982c52f26b68c4654e8b1e9821c1bd03e7f2
dedetive <ded31459@hotmail.com>
2025-05-13 13:05:40 -0300

    Remove outdated TravelTransition
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/RoomManager.java
+++ src/com/ded/misle/world/RoomManager.java
@@ -90,28 +90,6 @@ public class RoomManager {
         }
     }
 
-
-    public enum TravelTransition {
-//        LEAVING_TUANI_HOUSE_1(TUANI_CITY, 460, 483),
-//        ENTERING_TUANI_HOUSE_1(TUANI_HOUSE_1, 350, 110),
-//        TUANI_CITY_TO_1(TUANI_1, 300, 440),
-//        TUANI_1_TO_CITY(TUANI_CITY, 500, 31),
-//        TUANI_1_TO_2(TUANI_2, 700, 1000),
-//        TUANI_2_TO_1(TUANI_1, 1110, 31),
-
-        ;
-
-        public final int enteringRoomID;
-        public final int x;
-        public final int y;
-
-        TravelTransition(Room enteringRoom, int x, int y) {
-            this.enteringRoomID = enteringRoom.id;
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static Room roomIDToName(int roomID) {
         try {
             return rooms.get(roomID);
