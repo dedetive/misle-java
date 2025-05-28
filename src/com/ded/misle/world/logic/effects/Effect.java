3d31c24595962da093acb0ec9168955e95a804af
dedetive <ded31459@hotmail.com>
2025-05-28 17:47:39 -0300

    Add triggers on contact boolean to Effect
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/logic/effects/Effect.java
+++ src/com/ded/misle/world/logic/effects/Effect.java
@@ -5,7 +5,16 @@ import com.ded.misle.world.boxes.Box;
 public abstract class Effect {
     Effect() {}
 
+    private boolean triggersOnContact = true;
     public abstract void run(Box culprit, Box victim);
+
+    public boolean getTriggersOnContact() {
+        return triggersOnContact;
+    }
+    public Effect setTriggersOnContact(boolean triggersOnContact) {
+        this.triggersOnContact = triggersOnContact;
+        return this;
+    }
 
     @Override
     public String toString() {
