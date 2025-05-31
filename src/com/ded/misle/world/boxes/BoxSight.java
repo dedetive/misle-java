7ca625651c01dc96de225573d2375c08104868d6
dedetive <ded31459@hotmail.com>
2025-05-30 21:31:12 -0300

    Add BoxSight
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/world/boxes/BoxSight.java
@@ -0,0 +1,49 @@
+package com.ded.misle.world.boxes;
+
+import com.ded.misle.world.logic.Path;
+import com.ded.misle.world.logic.PhysicsEngine;
+
+import java.awt.*;
+
+public class BoxSight {
+    private final Box origin;
+    private Box target;
+    private boolean mustUpdate = true;
+    private boolean hasDirectSight = false;
+
+    public BoxSight(Box origin, Box target) {
+        this.origin = origin;
+        this.target = target;
+    }
+
+    public BoxSight(Box origin) {
+        this.origin = origin;
+    }
+
+    public boolean canSee(Box target) {
+        if (mustUpdate) {
+            Path pathToTarget = new Path(origin, target);
+
+            hasDirectSight = true;
+            for (Point point : pathToTarget.getPoints()) {
+                if (PhysicsEngine.isSpaceOccupied(point.x, point.y)) {
+                    hasDirectSight = false;
+                    break;
+                }
+            }
+        }
+
+
+        mustUpdate = false;
+        return hasDirectSight;
+    }
+
+    public boolean canSee() {
+        return canSee(this.target);
+    }
+
+    public void setTarget(Box target) {
+        this.target = target;
+        mustUpdate = true;
+    }
+}
