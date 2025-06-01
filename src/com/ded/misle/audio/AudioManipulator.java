992f90cb93fa1f8364d29aa137b0a7c0f76ad15f
dedetive <ded31459@hotmail.com>
2025-06-01 11:39:42 -0300

    Add AudioFile-scoped AudioManipulator instead of single clips
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/audio/AudioManipulator.java
+++ src/com/ded/misle/audio/AudioManipulator.java
@@ -1,26 +1,36 @@
 package com.ded.misle.audio;
 
+import javax.sound.sampled.BooleanControl;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
+import java.util.Arrays;
 
 public class AudioManipulator {
-    Clip clip;
+    Clip[] clips;
 
     public AudioManipulator(Clip clip) {
-        this.clip = clip;
+        this.clips = new Clip[] { clip };
+    }
+
+    public AudioManipulator(AudioFile file) {
+        this.clips = file.getAllClips();
     }
 
     public AudioManipulator setGain(float gain) {
-        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        float range = gainControl.getMaximum() - gainControl.getMinimum();
-        float value = (gain * range) + gainControl.getMinimum();
-        gainControl.setValue(value);
+        Arrays.stream(clips).iterator().forEachRemaining(clip -> {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float range = gainControl.getMaximum() - gainControl.getMinimum();
+            float value = (gain * range) + gainControl.getMinimum();
+            gainControl.setValue(value);
+        });
         return this;
     }
 
     public AudioManipulator setMute(boolean mute) {
-        BooleanControl booleanControl = (BooleanControl) clip.getControl(BooleanControl.Type.MUTE);
-        booleanControl.setValue(mute);
+        Arrays.stream(clips).iterator().forEachRemaining(clip -> {
+            BooleanControl booleanControl = (BooleanControl) clip.getControl(BooleanControl.Type.MUTE);
+            booleanControl.setValue(mute);
+        });
         return this;
     }
 }
