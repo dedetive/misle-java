ee3490877839297825e2c5ac9c82d3fcebc7a30c
dedetive <ded31459@hotmail.com>
2025-06-06 18:03:25 -0300

    Add Windows build.bat
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+REM Remove previous build folder
+rd /s /q out
+mkdir out
+
+echo Compiling...
+for /R src %%f in (*.java) do (
+    echo Compiling %%f
+)
+javac -d out -cp src @findstr /S /M /C:".java" src\*.java
+
+echo Copying resources...
+xcopy /E /I /Y src\resources out\resources
+mkdir out\resources\savefiles
+
+echo Building jar...
+cd out
+jar cfe Misle.jar com.ded.misle.Launcher -C . .
+
+echo.
+echo Build done! You can now run the game by double-clicking Misle.jar.
+pause
