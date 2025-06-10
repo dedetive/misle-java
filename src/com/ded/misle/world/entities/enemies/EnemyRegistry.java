8590e5cff88555aaec54fd83b54b5b3afb1cfb68
dedetive <ded31459@hotmail.com>
2025-06-10 14:54:35 -0300

    Add Javadocs to EnemyRegistry
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/entities/enemies/EnemyRegistry.java
+++ src/com/ded/misle/world/entities/enemies/EnemyRegistry.java
@@ -3,21 +3,46 @@ package com.ded.misle.world.entities.enemies;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A registry for managing all active {@link Enemy} instances in the game.
+ * This class provides static methods for adding, removing, listing, and clearing enemies.
+ */
 public class EnemyRegistry {
+    /**
+     * The list of all registered enemies currently in the world.
+     */
     private static final List<Enemy> enemies = new ArrayList<>();
 
+    /**
+     * Registers a new enemy into the global enemy list.
+     *
+     * @param enemy the {@link Enemy} instance to register
+     */
     public static void register(Enemy enemy) {
         enemies.add(enemy);
     }
 
+    /**
+     * Unregisters an enemy from the global enemy list.
+     *
+     * @param enemy the {@link Enemy} instance to remove
+     */
     public static void unregister(Enemy enemy) {
         enemies.remove(enemy);
     }
 
+    /**
+     * Returns a list of all currently registered enemies.
+     *
+     * @return a {@link List} of all {@link Enemy} instances
+     */
     public static List<Enemy> all() {
         return enemies;
     }
 
+    /**
+     * Clears all registered enemies from the registry.
+     */
     public static void clear() {
         enemies.clear();
     }
