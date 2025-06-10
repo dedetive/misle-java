1a04210b81600e8d2ddbecdc21f9acd4e22b2c7e
dedetive <ded31459@hotmail.com>
2025-06-10 11:43:23 -0300

    Add setCondition field to BehaviorCondition
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/entities/ai/BehaviorCondition.java
+++ src/com/ded/misle/world/entities/ai/BehaviorCondition.java
@@ -1,5 +1,8 @@
 package com.ded.misle.world.entities.ai;
 
+import java.util.function.Function;
+
 public interface BehaviorCondition {
     boolean matches(BehaviorContext context);
+    void setCondition(Function<BehaviorContext, Boolean> condition);
 }
