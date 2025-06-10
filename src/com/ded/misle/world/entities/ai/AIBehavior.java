cd10ec089e3ee89cbd181d31601f4e032e9a173d
dedetive <ded31459@hotmail.com>
2025-06-10 11:55:25 -0300

    Add setInterruptible to AIBehavior
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/entities/ai/AIBehavior.java
+++ src/com/ded/misle/world/entities/ai/AIBehavior.java
@@ -4,6 +4,7 @@ public interface AIBehavior extends BehaviorCondition {
 
     void tryExecute(BehaviorContext context);
     boolean isInterruptible();
+    void setInterruptible(boolean interruptible);
 
     void setPriority(int priority);
     int getPriority();
