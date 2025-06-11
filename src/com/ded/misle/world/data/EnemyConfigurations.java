613c8d2698ad02b962eb290247822c3f4c3b712e
dedetive <ded31459@hotmail.com>
2025-06-11 19:48:55 -0300

    Add Javadocs to EnemyConfigurations
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/data/EnemyConfigurations.java
+++ src/com/ded/misle/world/data/EnemyConfigurations.java
@@ -3,9 +3,17 @@ package com.ded.misle.world.data;
 import com.ded.misle.items.DropTable;
 import com.ded.misle.world.entities.ai.behaviors.PatrolBehavior;
 import com.ded.misle.world.entities.enemies.EnemyConfigurator;
+import com.ded.misle.world.entities.enemies.EnemyType;
 
 import java.awt.*;
 
+/**
+ * Centralized definitions of enemy setup configurations.
+ * Each constant provides a lambda that customizes stats, visuals,
+ * behavior, and loot for a particular type of enemy.
+ *
+ * These are used by {@link EnemyType}.
+ */
 public enum EnemyConfigurations {
     RED_BLOCK(enemy -> {
         enemy.setMaxHP(50);
@@ -18,6 +26,7 @@ public enum EnemyConfigurations {
         enemy.setCoinDropRange(0, 100);
         enemy.setCollision(true);
     }),
+
     GOBLIN(enemy -> {
         enemy.setMaxHP(20);
         enemy.fillHP();
@@ -42,6 +51,9 @@ public enum EnemyConfigurations {
             new PatrolBehavior(pathPoints));
     });
 
+    /**
+     * The functional configurator for this enemy setup.
+     */
     public final EnemyConfigurator c;
 
     EnemyConfigurations(EnemyConfigurator configurator) {
