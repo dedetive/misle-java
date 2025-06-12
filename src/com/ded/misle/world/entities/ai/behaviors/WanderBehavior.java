6b6c99426d53f8f5b3e856c0d618b1afd7e2c9b2
dedetive <ded31459@hotmail.com>
2025-06-12 17:44:06 -0300

    Add WanderBehavior (currently does nothing)
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/world/entities/ai/behaviors/WanderBehavior.java
@@ -0,0 +1,52 @@
+package com.ded.misle.world.entities.ai.behaviors;
+
+import com.ded.misle.world.entities.Entity;
+import com.ded.misle.world.entities.ai.BehaviorContext;
+import com.ded.misle.world.entities.ai.BehaviorType;
+import com.ded.misle.world.logic.Path;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class WanderBehavior extends AbstractBehavior {
+    private int maxDistanceFromOrigin;
+    private Path wanderingPath;
+    private WanderMode wanderMode;
+
+    WanderBehavior() {
+        this.maxDistanceFromOrigin = Integer.MAX_VALUE;
+        this.wanderMode = WanderMode.DISTANCE;
+    }
+
+    WanderBehavior(int maxDistanceFromOrigin) {
+        this.maxDistanceFromOrigin = maxDistanceFromOrigin;
+        this.wanderMode = WanderMode.DISTANCE;
+    }
+
+    WanderBehavior(Path wanderingPath) {
+        this.wanderingPath = wanderingPath;
+        this.wanderMode = WanderMode.PATH;
+    }
+
+    @Override
+    public void tryExecute(BehaviorContext context) {
+
+    }
+
+    @Override
+    public BehaviorType getType() {
+        return BehaviorType.WANDER;
+    }
+
+    public enum WanderMode {
+        /**
+         * Walks randomly within a confined path.
+         */
+        PATH,
+
+        /**
+         * Walks randomly within a max distance.
+         */
+        DISTANCE
+    }
+}
