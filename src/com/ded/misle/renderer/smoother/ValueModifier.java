816abce65122ecaba448aac7bac2643d7ad24dcc
dedetive <ded31459@hotmail.com>
2025-06-19 14:21:32 -0300

    Add ValueModifier
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/smoother/ValueModifier.java
@@ -0,0 +1,10 @@
+package com.ded.misle.renderer.smoother;
+
+/**
+ * Represents a temporary effect that may modify a value.
+ */
+public interface ValueModifier {
+    void update(float deltaTime);
+    float getOffset();
+    boolean isFinished();
+}
