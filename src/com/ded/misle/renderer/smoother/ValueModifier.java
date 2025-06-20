721f2d1c61c3b105af63e562cd97bf4797b7aa33
dedetive <ded31459@hotmail.com>
2025-06-20 17:48:25 -0300

    Make ValueModifier extend Cloneable
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/smoother/ValueModifier.java
+++ src/com/ded/misle/renderer/smoother/ValueModifier.java
@@ -3,7 +3,7 @@ package com.ded.misle.renderer.smoother;
 /**
  * Represents a temporary effect that may modify a value.
  */
-public interface ValueModifier {
+public interface ValueModifier extends Cloneable {
     void update(float deltaTime);
     float getOffset();
     boolean isFinished();
