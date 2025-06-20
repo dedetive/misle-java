33fcac850baa9970390572f05959b299e87b74bb
dedetive <ded31459@hotmail.com>
2025-06-20 18:11:05 -0300

    Add Javadocs to ValueModifier
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/smoother/ValueModifier.java
+++ src/com/ded/misle/renderer/smoother/ValueModifier.java
@@ -1,11 +1,45 @@
 package com.ded.misle.renderer.smoother;
 
 /**
- * Represents a temporary effect that may modify a value.
+ * Represents a temporary effect that modifies a value over time.
+ * <p>
+ * This interface is intended to be used with value smoothing systems
+ * such as {@link SmoothValue}, allowing dynamic visual behaviors
+ * like shaking, bouncing, or other temporal distortions to come.
+ * <p>
+ * Modifiers are additive and optional, and are removed once finished.
  */
 public interface ValueModifier extends Cloneable {
+
+    /**
+     * Updates the internal state of this modifier.
+     * This is usually used to progress time-dependent effects.
+     *
+     * @param deltaTime the time elapsed since the last update, in seconds
+     */
     void update(float deltaTime);
+
+    /**
+     * Returns the current offset this modifier contributes to the base value.
+     * This offset is additive and can be positive or negative.
+     *
+     * @return the current offset value applied by this modifier
+     */
     float getOffset();
+
+    /**
+     * Indicates whether this modifier has finished and should be removed.
+     *
+     * @return true if the modifier is finished, false otherwise
+     */
     boolean isFinished();
+
+    /**
+     * Creates a deep copy of this modifier.
+     * This is useful when assigning modifiers to independent value instances.
+     *
+     * @return a cloned copy of this modifier
+     * @throws CloneNotSupportedException if cloning is not supported
+     */
     ValueModifier clone() throws CloneNotSupportedException;
 }
