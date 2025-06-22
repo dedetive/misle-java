200ce59607ab65322528f75a326a7cdb399189bb
dedetive <ded31459@hotmail.com>
2025-06-21 22:44:45 -0300

    Update Setting class to enum
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/core/Setting.java
+++ src/com/ded/misle/core/Setting.java
@@ -1,47 +1,58 @@
 package com.ded.misle.core;
 
-import static com.ded.misle.core.LanguageManager.Language.en_US;
+import static com.ded.misle.core.LanguageManager.Language;
 
-public class Setting<T> {
+public enum Setting {
 
-    public Object value;
+    screenSize("medium"),
+    isFullscreen(true),
+    fullscreenMode("windowed"),
+    displayFPS(true),
+    antiAliasing(true),
+    frameRateCap(60),
+    languageCode(Language.en_US);
+
     public final Object defaultValue;
-    public final String name;
 
-    public Setting(T defaultValue, String name) {
+    private Object value;
+
+    Setting(Object defaultValue) {
         this.defaultValue = defaultValue;
-        this.name = name;
+        this.value = null;
+    }
+
+    // Getters
+
+    public Object get() {
+        return value != null ? value : defaultValue;
+    }
+
+    public void set(Object value) {
+        this.value = value;
     }
 
     public boolean bool() {
-        return Boolean.parseBoolean(String.valueOf(this.value));
-    }
-
-    public boolean boolDefault() {
-        return Boolean.parseBoolean(String.valueOf(this.value));
-    }
-
-    public String str() {
-        return String.valueOf(this.value);
-    }
-
-    public String strDefault() {
-        return String.valueOf(this.value);
+        return Boolean.parseBoolean(String.valueOf(get()));
     }
 
     public int integer() {
-        return Integer.parseInt(this.value.toString());
+        return Integer.parseInt(String.valueOf(get()));
     }
 
-    public static Setting<String> screenSize = new Setting<>("medium", "screenSize");
-    public static Setting<Boolean> isFullscreen = new Setting<>(true, "isFullscreen");
-    public static Setting<String> fullscreenMode = new Setting<>("windowed", "fullscreenMode");
-    public static Setting<Boolean> displayFPS = new Setting<>(true, "displayFPS");
-    public static Setting<Boolean> antiAliasing = new Setting<>(true, "antiAliasing");
-    public static Setting<Integer> frameRateCap = new Setting<>(60, "frameRateCap");
-    public static Setting<LanguageManager.Language> languageCode = new Setting<>(en_US, "language");
+    public String str() {
+        return String.valueOf(get());
+    }
 
-//    levelDesigner = Boolean.parseBoolean(getSetting("levelDesigner"));
-//    displayMoreInfo = getSetting("displayMoreInfo");
+    public String strDefault() {
+        return String.valueOf(defaultValue);
+    }
 
+    @SuppressWarnings("unchecked")
+    public <T> T getTyped(Class<T> clazz) {
+        Object val = get();
+        return clazz.isInstance(val) ? (T) val : null;
+    }
+
+    //    levelDesigner = Boolean.parseBoolean(getSetting("levelDesigner"));
+    //    displayMoreInfo = getSetting("displayMoreInfo");
 }
