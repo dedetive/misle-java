8b7bf004ddf97a563edb4ec5bdd976f215107ba5
dedetive <ded31459@hotmail.com>
2025-07-02 14:25:07 -0300

    Add PaletteCurator with generateFile method
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/image/PaletteCurator.java
@@ -0,0 +1,35 @@
+package com.ded.misle.renderer.image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.awt.*;
+import java.util.List;
+
+import static com.ded.misle.core.Path.*;
+
+public class PaletteCurator {
+
+    private PaletteCurator() {}
+
+    public static void generateFile(Palette palette, String fileName) {
+        Path path = getPath(PathTag.PALETTES).resolve(fileName + ".png");
+
+        BufferedImage image = new BufferedImage(palette.size(), 1, BufferedImage.TYPE_INT_ARGB);
+        List<Color> asList = palette.asList();
+
+        for (int i = 0; i < asList.size(); i++) {
+            image.setRGB(i, 0, asList.get(i).getRGB());
+        }
+
+        try {
+            Files.createDirectories(path.getParent());
+            ImageIO.write(image, "png", path.toFile());
+        } catch (IOException e) {
+            System.err.println("Failed to save palette image: " + e.getMessage());
+        }
+    }
+}
