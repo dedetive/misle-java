94fa068de90f8984a2b0aadbf2b1ce89a870a5cd
dedetive <ded31459@hotmail.com>
2025-07-02 14:58:59 -0300

    Add basic PaletteShifter
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/image/PaletteShifter.java
@@ -0,0 +1,18 @@
+package com.ded.misle.renderer.image;
+
+public class PaletteShifter {
+
+    private Palette palette;
+
+    public PaletteShifter(Palette palette) {
+        this.palette = palette;
+    }
+
+    public Palette getPalette() {
+        return palette;
+    }
+
+    public void setPalette(Palette palette) {
+        this.palette = palette;
+    }
+}
