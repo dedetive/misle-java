f0b368b2f7b80700ba306685990d5f5b03c72e60
dedetive <ded31459@hotmail.com>
2025-07-04 10:13:57 -0300

    Add basic 4 difficulties
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/world/data/Difficulty.java
@@ -0,0 +1,11 @@
+package com.ded.misle.world.data;
+
+public enum Difficulty {
+
+    // This is just WIP, I'll most likely change their names, and maybe add or remove some
+
+    EASY,
+    MEDIUM,
+    HARD,
+    NIGHTMARE
+}
