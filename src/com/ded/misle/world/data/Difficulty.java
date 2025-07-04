4e10ab1386c70c2143989dfeef8f43b5b1034b08
dedetive <ded31459@hotmail.com>
2025-07-04 10:39:02 -0300

    Add enemy stat multiplier to each difficulty
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/data/Difficulty.java
+++ src/com/ded/misle/world/data/Difficulty.java
@@ -4,8 +4,16 @@ public enum Difficulty {
 
     // This is just WIP, I'll most likely change their names, and maybe add or remove some
 
-    EASY,
-    MEDIUM,
-    HARD,
-    NIGHTMARE
+    EASY(0.75f),
+    MEDIUM(1f),
+    HARD(1.25f),
+    NIGHTMARE(1.75f)
+
+    ;
+
+    public final float enemyStatMultiplier;
+
+    Difficulty(float enemyStatMultiplier) {
+        this.enemyStatMultiplier = enemyStatMultiplier;
+    }
 }
