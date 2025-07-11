340b8297bdbb184ba169e6c06eedb1ab8e8df3b2
dedetive <ded31459@hotmail.com>
2025-07-11 14:35:50 -0300

    Add Lifetime particle modifier
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/particles/modifiers/Lifetime.java
@@ -0,0 +1,38 @@
+package com.ded.misle.renderer.particles.modifiers;
+
+import com.ded.misle.renderer.particles.Particle;
+import com.ded.misle.renderer.particles.ParticleModifier;
+
+public class Lifetime implements ParticleModifier {
+
+	private final long lifetime;
+	private long initialTime;
+
+	public Lifetime(long ms) {
+		this.lifetime = ms;
+	}
+
+	public Lifetime(float seconds) {
+		this((long) (seconds * 1000));
+	}
+
+	public static Lifetime ofSeconds(float seconds) {
+		return new Lifetime(seconds);
+	}
+
+	public static Lifetime ofMillis(long ms) {
+		return new Lifetime(ms);
+	}
+
+	public void start() {
+		this.initialTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public void modify(Particle particle) {
+		if (particle.isRunning() && initialTime == 0)
+			start();
+		if (System.currentTimeMillis() - initialTime > lifetime)
+			particle.destroy();
+	}
+}
