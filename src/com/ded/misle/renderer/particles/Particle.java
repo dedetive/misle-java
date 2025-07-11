de9ab003f0aa13c2b2412e5fccb372f8b3d45c4e
dedetive <ded31459@hotmail.com>
2025-07-10 23:57:28 -0300

    Add Particle.getDrawPos helper static method
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/particles/Particle.java
+++ src/com/ded/misle/renderer/particles/Particle.java
@@ -2,6 +2,9 @@ package com.ded.misle.renderer.particles;
 
 import java.awt.*;
 
+import static com.ded.misle.game.GamePanel.originalTileSize;
+import static com.ded.misle.game.GamePanel.player;
+
 public interface Particle {
 	void update();
 	void draw(Graphics2D g);
@@ -16,5 +19,11 @@ public interface Particle {
 
 	default boolean isRunning() {
 		return ParticleRegistry.contains(this);
+	}
+
+	static Point getDrawPos(Point origin) {
+		return new Point(
+				(int) (origin.x * originalTileSize - player.pos.getCameraOffsetX()),
+				(int) (origin.y * originalTileSize- player.pos.getCameraOffsetY()));
 	}
 }
