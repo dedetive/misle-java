9b695bd794ee14af71d8735ec741a95060383cb3
dedetive <ded31459@hotmail.com>
2025-07-13 14:04:03 -0300

    Add DestroyCondition particle modifier
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/particles/modifier/DestroyCondition.java
@@ -0,0 +1,26 @@
+package com.ded.misle.renderer.particles.modifier;
+
+import com.ded.misle.renderer.particles.core.*;
+
+import java.util.function.Predicate;
+
+@ModifierType(ModifierType.Type.DESTRUCTIVE)
+public class DestroyCondition implements ParticleModifier {
+
+	private final Predicate<Particle> condition;
+
+	public DestroyCondition(Predicate<Particle> condition) {
+		this.condition = condition;
+	}
+
+	@Override
+	public void modify(Particle particle) {
+		if (condition.test(particle))
+			particle.destroy();
+	}
+
+	@Override
+	public ActivationTime getActivationTime() {
+		return ActivationTime.FRAME;
+	}
+}
