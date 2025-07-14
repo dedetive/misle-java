67bc8925ed1b8062a3fd8e6cb60c8baf3377b753
dedetive <ded31459@hotmail.com>
2025-07-14 20:13:14 -0300

    Add Enlarge particle modifier
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/particles/modifier/Enlarge.java
@@ -0,0 +1,35 @@
+package com.ded.misle.renderer.particles.modifier;
+
+import com.ded.misle.renderer.particles.core.Particle;
+import com.ded.misle.renderer.particles.core.ParticleModifier;
+
+public class Enlarge implements ParticleModifier {
+	private final float max;
+	private final float step;
+	private float current = Float.MIN_VALUE;
+
+	public Enlarge(float step, float max) {
+		this.max = max;
+		this.step = step;
+	}
+
+	public Enlarge(float step) {
+		this(step, Float.MAX_VALUE);
+	}
+
+	public static Enlarge of(float step) {
+		return new Enlarge(step);
+	}
+
+	@Override
+	public void modify(Particle particle) {
+		if (current == Float.MIN_VALUE) current = particle.getSizeMulti();
+		current = Math.min(current + step, max);
+		particle.setSizeMulti(current);
+	}
+
+	@Override
+	public ActivationTime getActivationTime() {
+		return ActivationTime.FRAME;
+	}
+}
