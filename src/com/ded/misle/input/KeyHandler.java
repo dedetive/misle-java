33c21f85dd62301147090d6905cf88f3b2a1a4be
dedetive <ded31459@hotmail.com>
2025-07-17 11:31:20 -0300

    Update KeyHandler to itself implementing KeyListener
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/KeyHandler.java
+++ src/com/ded/misle/input/KeyHandler.java
@@ -2,28 +2,41 @@ package com.ded.misle.input;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class KeyHandler {
-	public static final KeyListener listener = new KeyListener() {
-		private static final Map<KeyEvent, Boolean> canPress = new HashMap<>();
+public class KeyHandler implements KeyListener {
+	private static final Set<Integer> heldKeys = new HashSet<>();
 
-		@Override
-		public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyTyped(KeyEvent e) {}
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if (canPress.computeIfAbsent(e, ev -> true)) KeyRegistry.trigger(e, KeyInputType.ON_PRESS);
-			canPress.put(e, false);
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if (!KeyRegistry.isValid(keyCode)) return;
+		KeyRegistry.trigger(e, KeyInputType.ON_PRESS);
+		setKeyHeld(keyCode, true);
+	}
 
-			KeyRegistry.trigger(e, KeyInputType.ON_HOLD);
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if (!KeyRegistry.isValid(keyCode)) return;
+		KeyRegistry.trigger(e, KeyInputType.ON_RELEASE);
+		setKeyHeld(keyCode, false);
+	}
+
+	public static void setKeyHeld(int keyCode, boolean held) {
+		if (held) heldKeys.add(keyCode);
+		else heldKeys.remove(keyCode);
+	}
+
+	public static void triggerAllHeld() {
+		for (Key key : KeyRegistry.getKeys()) {
+			if (key.keyInputType() == KeyInputType.ON_HOLD && heldKeys.contains(key.keyCode()) && !key.onCooldown()) {
+				key.action().execute();
+				key.lastTimeActivated = System.currentTimeMillis();
+			}
 		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-			KeyRegistry.trigger(e, KeyInputType.ON_RELEASE);
-			canPress.put(e, true);
-		}
-	};
+	}
 }
