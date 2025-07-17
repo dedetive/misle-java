18b53c29aac6a1e4522fc106a04e77d467ab6a8b
dedetive <ded31459@hotmail.com>
2025-07-17 10:33:51 -0300

    Rework Key class
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/Key.java
+++ src/com/ded/misle/input/Key.java
@@ -1,5 +1,64 @@
 package com.ded.misle.input;
 
-public record Key(int keyCode, Action action) {
+import java.util.Objects;
+
+public final class Key {
+	private final int keyCode;
+	private final Action action;
+	private final KeyInputType keyInputType;
+
+	private final long cooldown;
+	public long lastTimeActivated = 0;
+
+	public Key(int keyCode, Action action, KeyInputType keyInputType, long cooldown) {
+		this.keyCode = keyCode;
+		this.action = action;
+		this.keyInputType = keyInputType;
+		this.cooldown = cooldown;
+	}
+
+	public Key(int keyCode, Action action, KeyInputType keyInputType) {
+		this(keyCode, action, keyInputType, 0);
+	}
+
+	public int keyCode() {
+		return keyCode;
+	}
+
+	public Action action() {
+		return action;
+	}
+
+	public KeyInputType keyInputType() {
+		return keyInputType;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		var that = (Key) obj;
+		return this.keyCode == that.keyCode &&
+				Objects.equals(this.action, that.action) &&
+				Objects.equals(this.keyInputType, that.keyInputType);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(keyCode, action, keyInputType);
+	}
+
+	public boolean onCooldown() {
+		return System.currentTimeMillis() - lastTimeActivated > cooldown;
+	}
+
+	@Override
+	public String toString() {
+		return "Key{" +
+				"keyCode=" + keyCode + ", " +
+				"action=" + action + ", " +
+				"keyInputType=" + keyInputType + '}';
+	}
 
 }
