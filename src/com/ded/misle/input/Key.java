6a664e51010dedae4f9cdacdc7cd15e54979de19
dedetive <ded31459@hotmail.com>
2025-07-17 11:42:19 -0300

    Add parameterSupplier to Key
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/Key.java
+++ src/com/ded/misle/input/Key.java
@@ -1,32 +1,43 @@
 package com.ded.misle.input;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 public final class Key {
 	private final int keyCode;
 	private final Action action;
 	private final KeyInputType keyInputType;
+	private final Supplier<Object> parameterSupplier;
 
 	private final long cooldown;
 	public long lastTimeActivated = 0;
 
-	public Key(int keyCode, Action action, KeyInputType keyInputType, long cooldown) {
+	public Key(int keyCode, Action action, KeyInputType keyInputType, Supplier<Object> parameterSupplier, long cooldown) {
 		this.keyCode = keyCode;
 		this.action = action;
 		this.keyInputType = keyInputType;
+		this.parameterSupplier = parameterSupplier;
 		this.cooldown = cooldown;
 	}
 
-	public Key(int keyCode, Action action, KeyInputType keyInputType) {
-		this(keyCode, action, keyInputType, 0);
+	public Key(int keyCode, Action action, KeyInputType keyInputType, Supplier<Object> parameterSupplier) {
+		this(keyCode, action, keyInputType, parameterSupplier, 0);
 	}
 
 	public static Key of(int keyCode, Action action, KeyInputType keyInputType, long cooldown) {
-		return new Key(keyCode, action, keyInputType, cooldown);
+		return new Key(keyCode, action, keyInputType, null, cooldown);
 	}
 
 	public static Key of(int keyCode, Action action, KeyInputType keyInputType) {
-		return new Key(keyCode, action, keyInputType, 0);
+		return new Key(keyCode, action, keyInputType, null, 0);
+	}
+
+	public static Key of(int keyCode, Action action, KeyInputType keyInputType, Supplier<Object> parameterSupplier, long cooldown) {
+		return new Key(keyCode, action, keyInputType, null, cooldown);
+	}
+
+	public static Key of(int keyCode, Action action, KeyInputType keyInputType, Supplier<Object> parameterSupplier) {
+		return new Key(keyCode, action, keyInputType, null, 0);
 	}
 
 	public int keyCode() {
@@ -69,4 +80,7 @@ public final class Key {
 				"keyInputType=" + keyInputType + '}';
 	}
 
+	public Object parameter() {
+		return parameterSupplier == null ? null : parameterSupplier.get();
+	}
 }
