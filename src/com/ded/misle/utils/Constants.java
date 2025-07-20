6d79d926cc0351fed56e7033710f531b1e24811b
dedetive <ded31459@hotmail.com>
2025-07-20 13:35:30 -0300

    Add Javadocs to Constants
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/utils/Constants.java
+++ src/com/ded/misle/utils/Constants.java
@@ -1,6 +1,15 @@
 package com.ded.misle.utils;
 
+/**
+ * Utility class for storing global constants used across the game.
+ * <p>
+ * This class provides values that do not change at runtime and are
+ * referenced by multiple subsystems to ensure consistency.
+ */
 public final class Constants {
+	/**
+	 * Private constructor to prevent instantiation.
+	 */
 	private Constants() {}
 
 	public final static float DEFAULT_GAMMA_CORRECTION = 2.4f;
