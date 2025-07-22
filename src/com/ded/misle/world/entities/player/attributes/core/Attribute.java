e16273bc44866a1d9a80457efef0f120b0754356
dedetive <ded31459@hotmail.com>
2025-07-22 16:25:10 -0300

    Add separate active and passive attribute interfaces
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/entities/player/attributes/core/Attribute.java
+++ src/com/ded/misle/world/entities/player/attributes/core/Attribute.java
@@ -3,6 +3,12 @@ package com.ded.misle.world.entities.player.attributes.core;
 public interface Attribute<ValueType, ApplyType> {
 	void setValue(ValueType value);
 	ValueType getValue();
-	void update();
-	ApplyType apply(ApplyType arg);
+
+	interface ActiveAttribute<V, A> extends Attribute<V, A> {
+		A apply(A arg);
+	}
+
+	interface PassiveAttribute<V, A> extends Attribute<V, A> {
+		void update();
+	}
 }
