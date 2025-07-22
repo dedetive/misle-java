fd8274275c0e77f5055099298752bda9824c3653
dedetive <ded31459@hotmail.com>
2025-07-22 14:23:45 -0300

    Add setValue and getValue to Attribute
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/world/entities/player/attributes/core/Attribute.java
+++ src/com/ded/misle/world/entities/player/attributes/core/Attribute.java
@@ -1,6 +1,8 @@
 package com.ded.misle.world.entities.player.attributes.core;
 
 public interface Attribute<ValueType, ApplyType> {
+	void setValue(ValueType value);
+	ValueType getValue();
 	void update();
 	ApplyType apply(ApplyType arg);
 }
