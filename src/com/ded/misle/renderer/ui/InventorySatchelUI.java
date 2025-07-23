511ae6752457ba55270ccf96424b5d92b79e6b0b
dedetive <ded31459@hotmail.com>
2025-07-23 20:42:04 -0300

    Make InventorySatchelUI instantiable
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/ui/InventorySatchelUI.java
+++ src/com/ded/misle/renderer/ui/InventorySatchelUI.java
@@ -8,14 +8,14 @@ import static com.ded.misle.game.GamePanel.originalScreenHeight;
 import static com.ded.misle.game.GamePanel.originalScreenWidth;
 import static com.ded.misle.renderer.image.ImageManager.cachedImages;
 
-public class InventorySatchelUI {
-	private static boolean isActive = true;
+public final class InventorySatchelUI {
+	private boolean isActive = true;
 
-	public static void setActive(boolean isActive) {
-		InventorySatchelUI.isActive = isActive;
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
 	}
 
-	public static void drawIfPossible(Graphics2D g2d) {
+	public void drawIfPossible(Graphics2D g2d) {
 		if (!isActive) return;
 		g2d.drawImage(cachedImages.get(ImageManager.ImageName.INVENTORY_SATCHEL),
 				originalScreenWidth - 64,
