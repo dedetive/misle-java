a26cf0110f3b1224f5321a5f656a5aa84d3aea59
dedetive <ded31459@hotmail.com>
2025-07-25 12:32:48 -0300

    Add ActivePlayingMenu (almost empty)
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/menu/ActivePlayingMenu.java
@@ -0,0 +1,19 @@
+package com.ded.misle.renderer.menu;
+
+import com.ded.misle.renderer.ui.elements.InventorySatchelUI;
+import com.ded.misle.renderer.ui.core.UIRegistry;
+
+import java.awt.*;
+
+public class ActivePlayingMenu implements Menu {
+	private final UIRegistry registry = new UIRegistry();
+
+	public void draw(Graphics2D g2d) {
+		registry.drawActive(g2d);
+	}
+
+	@Override
+	public void init() {
+		registry.add(new InventorySatchelUI());
+	}
+}
