1fb7ecb9f2ccc0bb9cf3c95b98bf1647052f6c38
dedetive <ded31459@hotmail.com>
2025-07-25 12:22:48 -0300

    Modify InventorySatchelUI to extend SingletonUIElement and remove future redundancy
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/ui/InventorySatchelUI.java
+++ src/com/ded/misle/renderer/ui/InventorySatchelUI.java
@@ -8,19 +8,7 @@ import static com.ded.misle.game.GamePanel.originalScreenHeight;
 import static com.ded.misle.game.GamePanel.originalScreenWidth;
 import static com.ded.misle.renderer.image.ImageManager.cachedImages;
 
-public final class InventorySatchelUI implements UIElement {
-	private boolean isActive = true;
-
-	@Override
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-
-	@Override
-	public boolean isActive() {
-		return isActive;
-	}
-
+public final class InventorySatchelUI extends AbstractUIElement.SingletonUIElement {
 	@Override
 	public void drawIfPossible(Graphics2D g2d) {
 		if (!isActive) return;
@@ -28,18 +16,5 @@ public final class InventorySatchelUI implements UIElement {
 				originalScreenWidth - 64,
 				originalScreenHeight - 64,
 				null);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-
-		return this.getClass() == obj.getClass();
-	}
-
-	@Override
-	public int hashCode() {
-		return getClass().hashCode();
 	}
 }
