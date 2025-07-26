6ee7ed1b8fde08688338d71429522dd5569aadee
dedetive <ded31459@hotmail.com>
2025-07-26 17:30:55 -0300

    Add InputInteraction.checkValidity
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/interaction/InputInteraction.java
+++ src/com/ded/misle/input/interaction/InputInteraction.java
@@ -2,4 +2,5 @@ package com.ded.misle.input.interaction;
 
 public sealed interface InputInteraction permits KeyboardInteraction, MouseInteraction {
 	int keyCode();
+	default <T> boolean checkValidity(T args) { return true; }
 }
