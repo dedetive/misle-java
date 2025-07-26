f9806d3b74dd490c175d1b669920546749be088e
dedetive <ded31459@hotmail.com>
2025-07-26 16:59:35 -0300

    Change MouseButton keycode constants
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/interaction/MouseInteraction.java
+++ src/com/ded/misle/input/interaction/MouseInteraction.java
@@ -12,10 +12,10 @@ public record MouseInteraction(Rectangle pos, MouseButton button) implements Inp
 	}
 
 	public enum MouseButton {
-		NONE(1 + MOUSE_SERIAL_CODE),
-		LEFT(2 + MOUSE_SERIAL_CODE),
-		RIGHT(4 + MOUSE_SERIAL_CODE),
-		MIDDLE(8 + MOUSE_SERIAL_CODE);
+		NONE(0 + MOUSE_SERIAL_CODE),
+		LEFT(1 + MOUSE_SERIAL_CODE),
+		RIGHT(2 + MOUSE_SERIAL_CODE),
+		MIDDLE(3 + MOUSE_SERIAL_CODE);
 
 		public final int keyCode;
 
