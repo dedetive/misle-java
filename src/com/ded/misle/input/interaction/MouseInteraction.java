b94c1183b839e855c3e07d76a6941ac146618da4
dedetive <ded31459@hotmail.com>
2025-07-26 16:23:38 -0300

    Add random MOUSE_SERIAL_CODE
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/input/interaction/MouseInteraction.java
+++ src/com/ded/misle/input/interaction/MouseInteraction.java
@@ -1,18 +1,21 @@
 package com.ded.misle.input.interaction;
 
 import java.awt.*;
+import java.util.Random;
 
 public record MouseInteraction(Rectangle pos, MouseButton button) implements InputInteraction {
+	public static final int MOUSE_SERIAL_CODE = new Random().nextInt();
+
 	@Override
 	public int keyCode() {
 		return button.keyCode;
 	}
 
 	public enum MouseButton {
-		NONE(1),
-		LEFT(2),
-		RIGHT(4),
-		MIDDLE(8);
+		NONE(1 + MOUSE_SERIAL_CODE),
+		LEFT(2 + MOUSE_SERIAL_CODE),
+		RIGHT(4 + MOUSE_SERIAL_CODE),
+		MIDDLE(8 + MOUSE_SERIAL_CODE);
 
 		public final int keyCode;
 
