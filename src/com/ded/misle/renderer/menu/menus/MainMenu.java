d65d157e4708492b075c35020df2373a5dd76a2b
dedetive <ded31459@hotmail.com>
2025-07-28 14:09:57 -0300

    Add Quit button to MainMenu
    Signed-off-by: dedetive <ded31459@hotmail.com>

--- src/com/ded/misle/renderer/menu/menus/MainMenu.java
+++ src/com/ded/misle/renderer/menu/menus/MainMenu.java
@@ -1,6 +1,9 @@
 package com.ded.misle.renderer.menu.menus;
 
 import com.ded.misle.core.LanguageManager;
+import com.ded.misle.game.GamePanel;
+import com.ded.misle.input.*;
+import com.ded.misle.input.interaction.MouseInteraction;
 import com.ded.misle.renderer.menu.core.Menu;
 import com.ded.misle.renderer.smoother.SmoothValue;
 import com.ded.misle.renderer.smoother.modifiers.SineWaveModifier;
@@ -12,6 +15,7 @@ import com.ded.misle.renderer.ui.elements.Title;
 import java.awt.*;
 
 import static com.ded.misle.game.GamePanel.*;
+import static com.ded.misle.renderer.MainRenderer.gameStart;
 
 public class MainMenu implements Menu {
 	private final static Rectangle START_BUTTON_RECTANGLE = new Rectangle(
@@ -19,10 +23,16 @@ public class MainMenu implements Menu {
 			129,
 			128,
 			30);
+	private final static Rectangle QUIT_BUTTON_RECTANGLE = new Rectangle(
+			(originalScreenWidth - 128) / 2 + 68,
+			129 + 35,
+			60,
+			30);
 
 	private final UIRegistry registry = new UIRegistry();
-	private final Title title = new Title(LanguageManager.getText("misle"));
+	private final Title title = new Title(LanguageManager.getText("misle")).setRainbowness(0.005f);
 	private final Button startButton = new Button(LanguageManager.getText("START"), START_BUTTON_RECTANGLE);
+	private final Button quitButton = new Button(LanguageManager.getText("quit"), QUIT_BUTTON_RECTANGLE);
 	private final SmoothValue smoothTitleScale = new SmoothValue(1.6f);
 	private final SmoothValue smoothTitleRotation = new SmoothValue(0f);
 
@@ -45,8 +55,19 @@ public class MainMenu implements Menu {
 		smoothTitleRotation.addModifiers(
 				new SineWaveModifier(4f, 0.6f)
 		);
+		KeyRegistry.addKey(startButton.addFunction(new KeyBuilder(
+				MouseInteraction.of(START_BUTTON_RECTANGLE, MouseInteraction.MouseButton.LEFT),
+				new Action(() -> gameStart(1), (ignored) -> true, false),
+				KeyInputType.ON_RELEASE
+		)));
+		KeyRegistry.addKey(quitButton.addFunction(new KeyBuilder(
+				MouseInteraction.of(QUIT_BUTTON_RECTANGLE, MouseInteraction.MouseButton.LEFT),
+				new Action(GamePanel::quitGame, (ignored) -> true, false),
+				KeyInputType.ON_RELEASE
+		)));
 		registry.add(title);
 		registry.add(startButton);
+		registry.add(quitButton);
 		registry.add(new Button(LanguageManager.getText("uwu"), new Rectangle(originalScreenWidth / 2 - 64, originalScreenHeight - 30, 128, 20)));
 	}
 }
