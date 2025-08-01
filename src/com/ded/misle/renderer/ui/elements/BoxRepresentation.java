c4d76b9eca896f97b722c30c9b16bb5b18db7371
dedetive <ded31459@hotmail.com>
2025-08-01 18:25:22 -0300

    Add BoxRepresentation UI element
    Signed-off-by: dedetive <ded31459@hotmail.com>

---
+++ src/com/ded/misle/renderer/ui/elements/BoxRepresentation.java
@@ -0,0 +1,34 @@
+package com.ded.misle.renderer.ui.elements;
+
+import com.ded.misle.renderer.ui.core.AbstractUIElement;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static com.ded.misle.game.GamePanel.originalTileSize;
+
+public class BoxRepresentation extends AbstractUIElement {
+	private BufferedImage texture;
+	private Point position;
+
+	public BoxRepresentation() {
+		BoxScreen.addBox(this);
+	}
+
+	public BoxRepresentation setPosition(Point position) {
+		this.position = position;
+		return this;
+	}
+
+	public BoxRepresentation setTexture(BufferedImage texture) {
+		this.texture = texture;
+		return this;
+	}
+
+	@Override
+	public void drawIfPossible(Graphics2D g2d) {
+		if (texture == null || position == null) return;
+
+		g2d.drawImage(texture, position.x, position.y, originalTileSize, originalTileSize, null);
+	}
+}
